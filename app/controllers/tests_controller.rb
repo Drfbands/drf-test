e0f4cdddb8a227178383712616d486185e83f6bc
Brian Jones <drf0124@yahoo.com>
2016-01-25 22:15:07 -0500

    added congugate method file to see if we can push it and videos to heroku

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,13 @@
 class TestsController < ApplicationController
   def home
   end
+
+  def tran
+  		@exercises = ["Squat", "Jumps", "Forward_run", "Backward_run"]
+		@exercise_now = @exercises.sample(1)
+		@squat_reaction = "https://www.youtube.com/embed/4Vsl92kpFw0"
+		@jumps_reaction = "This is a jump"
+		@forward_run_reaction = "This is a forward run"
+		@backward_run_reaction = "This is a backward run"
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'tests#home'
 
+  get 'tests/tran'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
